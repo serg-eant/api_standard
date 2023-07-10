6af9d3144678359c459e8fa483455d9eb26ed1d0
serg-eant <kuvshinsergeic@gmail.com>
2023-07-10 10:00:00 +0300

    initial commit

---
+++ src/app/models.py
@@ -0,0 +1,25 @@
+import datetime
+
+from sqlalchemy import DateTime, func
+from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+from app.core.config import settings
+
+
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
+
+class Examples(Base):
+    __tablename__ = "examples"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    description: Mapped[str | None]
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=settings.timezone), server_default=func.now()
+    )
+    updated_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=settings.timezone), onupdate=func.now()
+    )
